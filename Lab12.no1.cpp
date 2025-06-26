3b7a08af9c9ee281deb94740004bcb379398b730
Brain17 <b.daminov@newuu.uz>
2025-06-26 16:57:20 +0500

    Add files via upload

---
+++ Lab12.no1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int a,b,c,d,r;
+  cin>>a>>b>>c>>d>>r;
+  if(a>=0||b>=0||c>=0||d>=0||r>=0){
+    cout<< 1<<endl;
+   } else{
+     cout <<0<<endl;
+   }
+  
+    return 0;
+}
